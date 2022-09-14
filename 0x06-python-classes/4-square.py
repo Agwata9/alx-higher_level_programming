a9faceb6ebc8f13bd07cfbef9b67cb454d50b64d
agwata9 <agwata9@gmail.com>
2022-09-14 15:25:41 -0700

    submission

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -1,20 +1,48 @@
 #!/usr/bin/python3
+# -*- coding: utf-8 -*-
+
+"""
+a class Square that defines a square by: (based on 3-square.py).
+"""
+
+
 class Square:
+
+    """
+a class Square that defines a square by: (based on 3-square.py).
+"""
+
     def __init__(self, size=0):
+        """
+        Private instance attribute: size Instantiation with size.
+        """
+
         self.size = size
 
     @property
     def size(self):
+        """
+        Private instance attribute: size Instantiation with size.
+        """
+
         return self.__size
 
     @size.setter
     def size(self, value):
+        """
+        Private instance attribute: size Instantiation with size.
+        """
+
         if type(value) != int:
-            raise TypeError("size must be an integer")
+            raise TypeError('size must be an integer')
         elif value < 0:
-            raise ValueError("size must be >= 0")
+            raise ValueError('size must be >= 0')
         self.__size = value
 
     def area(self):
+        """
+        Private instance attribute: a Instantiation with size.
+        """
+
         a = self.__size * self.__size
-        return
+        return a
