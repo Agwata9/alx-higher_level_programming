2b37d98a06ffa7706f7f1a86981849e40f84b2b3
agwata9 <agwata9@gmail.com>
2022-09-14 11:17:22 -0700

    commit

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,4 +1,6 @@
 #!/usr/bin/python3
 class Square:
+    """a class Square that defines a square by: (based on 0-square.py)."""
     def __init__(self,size):
+        """Private instance attribute: size Instantiation with size."""
         self.__size=size
--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -1,13 +1,16 @@
 #!/usr/bin/python3
 class Square:
+    """a class Square that defines a square by: (based on 0-square.py)."""
     def __init__(self, size=0):
+        """Private instance attribute: size Instantiation with size."""
         if type(size) != int:
             raise TypeError("size must be an integer")
         elif size < 0:
             raise ValueError("size must be >= 0")
 
         self.__size = size
-        
-        def area(self):
-            area = self.__size*self.__size
+
+    def area(self):
+        """Public instance method:"""
+        area = self.__size*self.__size
         return area
