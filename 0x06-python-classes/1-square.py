be6f221cd8ba9103a0fab5bccc75fead15c21172
agwata9 <agwata9@gmail.com>
2022-09-14 15:00:01 -0700

    submision

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -7,12 +7,10 @@ class Square:
     """
     a class Square that defines a square by: (based on 0-square.py).
     """
-    
+
     def __init__(self,size):
         """
         Private instance attribute: size Instantiation with size.
         """
 
         self.__size=size
-
-if __name__== "__main__":
