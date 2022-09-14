1e1674f9a5579cd91f3567890e3543ed5f0022ad
agwata9 <agwata9@gmail.com>
2022-09-14 15:04:57 -0700

    submision

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -5,7 +5,7 @@ a class Square that defines a square by: (based on 0-square.py).
 
 class Square:
     """
-    a class Square that defines a square by: (based on 0-square.py).
+    __doc__:a class Square that defines a square.
     """
 
     def __init__(self,size):
