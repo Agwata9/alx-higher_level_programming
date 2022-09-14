0ebb997286737820acce7299d1734af897bd36f4
agwata9 <agwata9@gmail.com>
2022-09-14 15:41:21 -0700

    submit

--- 0x06-python-classes/5-square.py
+++ 0x06-python-classes/5-square.py
@@ -1,14 +1,41 @@
 #!/usr/bin/python3
+# -*- coding: utf-8 -*-
+
+"""
+a class Square that defines a square by: (based on 4-square.py).
+"""
+
+
 class Square:
+    
+    """
+    a class Square that defines a square by: (based on 3-square.py).
+    """
+    
     def __init__(self, size=0):
+        
+        """
+        Private instance attribute: size Instantiation with size.
+        """
+        
         self.size = size
 
     @property
     def size(self):
+        
+        """
+        Private instance attribute: size Instantiation with size.
+        """
+        
         return self.__size
 
     @size.setter
     def size(self, value):
+        
+        """
+        Private instance attribute: size Instantiation with size.
+        """
+        
         if type(value) != int:
             raise TypeError("size must be an integer")
         elif value < 0:
@@ -16,10 +43,20 @@ class Square:
         self.__size = value
 
     def area(self):
+        
+        """
+        Private instance attribute: size Instantiation with size.
+        """
+        
         a = self.__size * self.__size
         return a
 
     def my_print(self):
+        
+        """
+        Private instance attribute: size Instantiation with size.
+        """
+        
         if self.__size == 0:
             print("")
         for i in range(0, self.__size):
