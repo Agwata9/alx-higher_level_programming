5d52025ff56bac1eb82ea3c2071628926df77226
agwata9 <agwata9@gmail.com>
2022-09-26 14:28:14 -0700

    submision

--- 0x0A-python-inheritance/6-base_geometry.py
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -4,4 +4,8 @@
 a class BaseGeometry (based on 5-base_geometry.py).
 """
 
+class BaseGeometry:
+    """Class with public instance method"""
 
+    def area(self):
+        raise Exception('area()is not implemented')
