de0dfac146cc58594e31164e6de0dc73b71ca94e
agwata9 <agwata9@gmail.com>
2022-09-26 14:21:15 -0700

    submission

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -1,15 +1,12 @@
 #!/usr/bin/python3
+"""Module 1-my_list.
+Creates a class MyList that inherits from the list class.
 """
-a class MyList that inherits from list
-"""
+
+"""Class MyList inherits from list."""
 
 
 class MyList(list):
-    ''' Represents a MyList
-    '''
-
     def print_sorted(self):
-        '''
-        prints the list, but sorted
-        '''
-        print(sorted(self))
+        newList = sorted(self[:])
+        print("{}".format(newList))
