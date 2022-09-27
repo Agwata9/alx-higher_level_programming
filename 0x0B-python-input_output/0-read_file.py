3a6806b6345b8cf913c6581080965e4ebf518b0d
agwata9 <agwata9@gmail.com>
2022-09-27 11:25:20 -0700

    submision

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -4,11 +4,12 @@
 a function that reads a text file (UTF8)
 """
 
-def read_file(filename=""):
-    """a function that reads a text file (UTF8)
-    and prints it to stdout
-    """
 
+def read_file(filename=""):
+    """ Read a UTF8 text file and
+        print it to stdout.
+        param: filename - the file to open and read.
+    """
     with open(filename, "r") as f:
         text = f.read()
         for line in text:
