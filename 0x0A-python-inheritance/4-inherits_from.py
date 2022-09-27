8d68328da1c5d03e73e2c170c75057f71b75fdf3
agwata9 <agwata9@gmail.com>
2022-09-27 13:29:22 -0700

    submission

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -1,14 +1,15 @@
 #!/usr/bin/python3
-"""a function that returns True if the object is an instance of
+"""a function that returns True if the object
 a class that inherited (directly or indirectly)
 from the specified class ; otherwise False
 """
 
 
 def inherits_from(obj, a_class):
-    """the object is an instance of a class that inherited (directly or indirectly)
+    """the object is an instance of a class that inherited
     obj: an object
     a_class: inherit_from
     """
-
-    return isinstance(obj, a_class) and type(obj) is not a_class
+    if issubclass(type(obj), a_class) and type(obj) != a_class:
+        return True
+    return False
--- 0x0A-python-inheritance/6-base_geometry.py
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -1,12 +1,10 @@
 #!/usr/bin/python3
-"""
-a class BaseGeometry (based on 5-base_geometry.py).
-"""
+"""a class BaseGeometry (based on 5-base_geometry.py)."""
+
 
 class BaseGeometry:
     """Class with public instance method"""
-
+    
     def area(self):
         """raise Exception"""
-
         raise Exception("area()is not implemented")
