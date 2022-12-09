a5a989ab3361efff4ebfd4ab5f4d737530c26696
agwata9 <agwata9@gmail.com>
2022-12-09 12:54:03 -0800

    submission

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -1,17 +1,17 @@
 #!/usr/bin/python3
 
 """
-    A script that lists all states from the database hbtn_0e_0_usa
-    starting with capital letter N
-    Username, password and database names are given as user args
+A script that lists all states from the database hbtn_0e_0_usa
+starting with capital letter N
+Username, password and database names are given as user args
 """
-
 
 import sys
 import MySQLdb
 
 
 if __name__ == '__main__':
+    
     db = MySQLdb.connect(user=sys.argv[1],
                          passwd=sys.argv[2],
                          db=sys.argv[3],
