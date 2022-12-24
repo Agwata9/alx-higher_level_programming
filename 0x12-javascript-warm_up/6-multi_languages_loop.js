abde2e9782c77f22e94c1ca49afa40977856bb9f
agwata9 <agwata9@gmail.com>
2022-12-24 12:19:01 -0800

    correction

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,6 +1,10 @@
 #!/usr/bin/node
-#a script that prints 3 lines
 
-for (const line of ['C is fun', 'Python is cool', 'JavaScript is amazing']) {
-	console.log(line);
+// script to print strings using a loop
+
+let i;
+const myString = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
+
+for (i = 0; i < 3; i++) {
+  console.log(myString[i]);
 }
